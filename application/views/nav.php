1ccc77584dc500a4d7d7ab7836a7650617712cb7
AbhilashK <37733165+Kyuboyo@users.noreply.github.com>
2025-01-11 15:20:13 +0530

    home nav

--- application/views/nav.php
+++ application/views/nav.php
@@ -1,6 +1,6 @@
 <nav class="navbar navbar-light bg-light">
   <div class="container-fluid">
-    <a class="navbar-brand" href="#">User Management</a>
+    <?php echo anchor("home", "User Management", ['class'=>'navbar-brand']); ?>
     <?php echo anchor("home/create", "Add User", ['class'=>'btn btn-outline-success']); ?>
   </div>
 </nav>
